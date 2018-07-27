86d67549af4cb1b2ca028634e73d807faae24e7b
chaosflaw <jpagranz@gmail.com>
2018-07-27 11:53:00 +0100

    Added POST /users and generateToken method

--- server/server.js
+++ server/server.js
@@ -10,19 +10,19 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 
-
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
 
-    todo.save().then((doc) => {
-        res.send(doc);
+    todo.save().then((todo) => {
+        res.send(todo);
     }, (e) => {
         res.status(400).send(e);
     });
@@ -102,6 +102,19 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 
 
